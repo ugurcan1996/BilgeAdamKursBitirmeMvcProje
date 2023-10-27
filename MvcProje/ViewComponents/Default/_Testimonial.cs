0226ea9f853092212d17596592bb381db224c3c5
Ugur CAN <ugr.can96@gmail.com>
2023-10-27 22:30:07 +0300

    27102023

---
+++ MvcProje/ViewComponents/Default/_Testimonial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcProje.ViewComponents.Default
+{
+    public class _Testimonial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
