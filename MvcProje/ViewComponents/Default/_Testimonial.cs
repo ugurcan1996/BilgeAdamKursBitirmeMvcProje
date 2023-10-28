5ddef4f728299cab74fa28bd0ce1d9fa821ae8da
Ugur CAN <ugr.can96@gmail.com>
2023-10-28 20:50:35 +0300

    28102023

--- MvcProje/ViewComponents/Default/_Testimonial.cs
+++ MvcProje/ViewComponents/Default/_Testimonial.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MvcProje.ViewComponents.Default
 {
     public class _Testimonial: ViewComponent
     {
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = testimonialManager.TGetList();
+            return View(values);
         }
     }
 }
